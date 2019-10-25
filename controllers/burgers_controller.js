a65eaec94a37900c9d6d1198a96102276ca4281b
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-25 17:58:34 -0500

    Configured default route to export burger info

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,14 @@ var burger = require("../models/burger");
 var router = express.Router();
 
 router.use(function(req, res) {
-    res.render("index");
+    burger.selectAll(function(data) {
+
+        var burgersObject = {
+            burgers:  data
+        }
+
+        res.render("index", burgersObject);
+    })
 })
 
 module.exports = router;
