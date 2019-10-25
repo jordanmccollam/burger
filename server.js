a5f1e13842af7de43d741f13a064b8c4014fb59c
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-25 17:20:56 -0500

    Configured handlebars

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var handlebars = require("express-handlebars");
 
 var PORT = process.env.PORT || 8080;
 
@@ -8,4 +9,11 @@ var app = express();
 // parse config
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Handlebars Config
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes to give server acess to them
+
 
