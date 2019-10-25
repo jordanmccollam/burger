afd0e2fa6c490f88a0ad9392f41496f31cc3a652
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-25 16:55:59 -0500

    Added callback functionality

--- config/orm.js
+++ config/orm.js
@@ -2,16 +2,17 @@ var connection = require("./connection");
 
 var orm = {
 
-    selectAll: function (table) {
+    selectAll: function (table, callback) {
         var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [table], function(err, result) {
+        connection.query(queryString, [table], function (err, result) {
             if (err) throw err;
             console.table(result);
+            callback(result);
         })
     },
 
     // insertOne: function () {
-    //     var queryString = "";
+    //     var queryString = ""; 
     // },
 
     // updateOne: function () {
--- models/burger.js
+++ models/burger.js
@@ -2,4 +2,12 @@ var orm = require("../config/orm");
 
 // CREATE the code that will call the ORM functions using burger specific input
 
-orm.selectAll("burgers");
+var burger = {
+
+    selectAll: function(callback) {
+        orm.selectAll("burgers", function(res) {
+            callback(res);
+        });
+    },
+
+}
