3ee4b43f28c37a8baf5f6283b7031810c7138b89
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-25 16:37:14 -0500

    made connect function

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,18 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3360,
+    port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
 });
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+})
 
 module.exports = connection;
