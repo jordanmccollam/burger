6e6b065d5692c33bd641ef691a13d68c1d10c6bb
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-25 15:57:54 -0500

    Added SQL connection

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,9 @@
 var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3360,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+})
